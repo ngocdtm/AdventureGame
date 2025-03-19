beee17104e58d9005b3f78886244ebb4b68b6240
Acer <vtharu2004@gmail.com>
2025-03-19 23:05:49 +0700

    Strategy design pattern

--- Adventure/src/entity/Entity.java
+++ Adventure/src/entity/Entity.java
@@ -18,7 +18,7 @@ import strategy.MovementStrategy;
 
 public abstract class Entity 
 {
-	GamePanel gp;
+	public GamePanel gp;
 	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
 	public BufferedImage attackUp1, attackUp2, attackDown1, attackDown2,
 	attackLeft1, attackLeft2, attackRight1, attackRight2,
